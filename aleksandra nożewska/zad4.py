bb64c1b6bba74f035383840b1d89fa63fc1772c7
onozewska7 <148948367+onozewska7@users.noreply.github.com>
2023-12-17 20:45:18 +0100

    Poprawki
    Poprawiono wsyzstkie programy by działały na funkcjach

--- aleksandra nożewska/zad4.py
+++ aleksandra nożewska/zad4.py
@@ -1,11 +1,14 @@
-a=0
-print(a)
-b=1
-print(b)
-n=100
+def zad4():
+    a=0
+    print(a)
+    b=1
+    print(b)
+    n=100
 
-for i in range(n-1):
-    c=a+b
-    print(c)
-    a=b
-    b = c
+    for i in range(n-1):
+        c=a+b
+        print(c)
+        a=b
+        b = c
+
+zad4()
